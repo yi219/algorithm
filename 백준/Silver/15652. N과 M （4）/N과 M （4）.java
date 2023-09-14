6f65576e3951cf8c35a958dbc9648678d7d02a67
예인 <yi219dev@gmail.com>
2023-09-14 16:38:24 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 19708 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,60 +1,54 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.channels.NonWritableChannelException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+	static boolean[] numCount;
+	static int n;
+	static int m;
+	static StringBuilder sb;
+    static int[] answerArray;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuffer sb = new StringBuffer();
-    static StringBuffer setSb = new StringBuffer();
-    static StringTokenizer st;
-
-    static int n, m;
-
-    static int[] inputIntArr;
-
-
-    public static void input() throws IOException{
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        inputIntArr = new int[n];
-        for (int i = 1 ; i <= n; i++) inputIntArr[i - 1] = i;
-    }
-
-    public static void pro() {
-
-        backtracking(0, new int[m]);
-        System.out.println(sb);
-
-    }
-
-    public static void backtracking(int depth, int[] elements) {
-
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(elements[i]).append(' ');
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		sb = new StringBuilder();
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+        answerArray = new int[m];
+		numCount = new boolean[n];
+		
+		nKWAm(0,0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		
+	}
+	
+	public static void nKWAm(int depth, int startindex) {
+        if(depth == m){
+            for(int e : answerArray){
+                sb.append(e + " ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
-
-        for (int i = 0; i < n; i++) {
-        	
-        	if (depth != 0) {
-        		if (elements[depth - 1] > inputIntArr[i]) continue;
-        	}
-            elements[depth] = inputIntArr[i];
-            backtracking(depth + 1, elements);
+        for(int i = startindex;i<numCount.length;i++){
+            numCount[i] = true;
+            answerArray[depth] = i+1;
+            nKWAm(depth+1,i);
         }
-    }
-
-
-
-    public static void main(String[] args) throws IOException{
-        input();
-        pro();
-    }
-
+	}
 }
