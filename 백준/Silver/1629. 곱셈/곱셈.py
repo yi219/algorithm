4009b61ffcde79160fe3b8bcc13875e9d0a74d1b
예인 <yi219dev@gmail.com>
2023-12-14 04:08:44 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+A, B, C = map(int, input().split())
+
+def power(a,b,m):
+    result = 1
+    while b > 0:
+        if b % 2 != 0:
+            result = (result * a) % m
+        b //= 2
+        a = (a * a) % m
+
+    return result
+
+
+print(power(A, B, C))
